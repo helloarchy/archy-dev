191e33a3b9dfee8974c2f60c9a7b8a0dbbaf75a3
Robert Hardy <helloarchy@gmail.com>
2019-06-19 21:42:32 +0100

    Initial commit.

---
+++ Models/ProjectItem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Archy_Dev.Models
+{
+    public class ProjectItem
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string NameNormalised { get; set; }
+        public string ShortDescription { get; set; }
+        public string ProgrammingLanguages { get; set; }
+        public string KeyWords { get; set; }
+        public string ThumbnailImage { get; set; }
+        public bool IsVisible { get; set; }
+        public string Repository { get; set; }
+        public string Controller { get; set; }
+    }
+
+    public class ProjectItemDBContext : DbContext
+    {
+        public DbSet<ProjectItem> ProjectItems { get; set; }
+    }
+}
